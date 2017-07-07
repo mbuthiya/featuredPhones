ffc2f4eb02eaf936ff945ac28b398ad35dd2357a
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-07-07 11:31:35 +0300

    Create a get input function Created a function to get user input

--- phones.py
+++ phones.py
@@ -1,4 +1,9 @@
-my_keys = {1:["a","d","g","j","m","p","t","w",' '],
-2:['b','e','h','k','n','q','u','x'],
-3:['c','f','i','l','o','r','v','y'],
-4:['s','z']}
+my_buttons = {"1":["a","d","g","j","m","p","t","w",' '],
+"2":['b','e','h','k','n','q','u','x'],
+"3":['c','f','i','l','o','r','v','y'],
+"4":['s','z']}
+
+def get_input():
+    print("Enter your sentence")
+    user_input = input().lower()
+    return user_input
